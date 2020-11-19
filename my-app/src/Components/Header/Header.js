b7544d18b82f96366344edc02b8d2f83d28d557b
Tigran <b.tigran@mail.com>
2020-11-20 02:21:31 +0300

    Release 1

---
+++ my-app/src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Header.css'
+export default class Header extends Component {
+  render() {
+    return (
+      <div className='Header' >
+        Промокоды для каршеринга Ноябрь 2020
+      </div>
+    )
+  }
+}
---
+++ my-app/src/Components/PromoCar/PromoCar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import './PromoCar.css'
+
+export default class PromoCar extends Component {
+  render() {
+    return (
+      <div className='Promocods'>
+
+      <div className='PromoCar'>
+        ЯндексДрайв
+        <br></br>
+        <h3>ПРОМОКОД</h3>
+        
+      </div>
+       <div className='PromoCar'>
+       Делимобиль
+        <br></br>
+        <h3>ПРОМОКОД</h3>
+       
+     </div>
+     <div className='PromoCar'>
+     BelkaCar
+        <br></br>
+        <h3>ПРОМОКОД</h3>
+       
+     </div>
+      </div>
+    )
+  }
+}
